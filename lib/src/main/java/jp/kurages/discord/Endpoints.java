6208455125a8d45fbdfbae46cbc5b4ac3990b867
Yui Tanabe <tianbianyouwei292@gmail.com>
2023-04-23 22:20:21 +0900

    feat(endpoint)!: add and replacing endpoint
    Add Gateway/Gateway Bot endpoint for WebSocket
    
    BREAKING CHANGE: This replaces how to build endpoints

--- lib/src/main/java/jp/kurages/discord/Endpoints.java
+++ lib/src/main/java/jp/kurages/discord/Endpoints.java
@@ -5,15 +5,33 @@ public class Endpoints {
 	/** 認証URL {@value} */
 	public static final String AUTHORIZATION = "https://discord.com/oauth2/authorize";
 
+	public static final String channels(String id) {
+		return BASE_API + "/channels/" + id;
+	}
+	public static final String channels() {
+		return BASE_API + "/channels";
+	}
+	public static final String users(String id) {
+		return BASE_API + "/users/" + id;
+	}
+	public static final String users() {
+		return BASE_API + "/users";
+	}
+	public static final String guilds(String id) {
+		return BASE_API + "/guilds/" + id;
+	}
+	public static final String guilds() {
+		return BASE_API + "/guilds";
+	}
 	/** CDN URL {@value} */
 	public static final String CDN_URL = "https://cdn.discordapp.com";
-
-	/** CDN アバター取得URL 0: userId 1: avatarHash */
-	public static final String AVATAR_URL = CDN_URL + "/avatars/{0}/{1}";
+	public static final String GATEWAY = "/gateway";
+	public static final String GATEWAY_BOT = "/gateway/bot";
 
 	// auth
 	/** OAuth2 Token取得 {@value} */
 	public static final String TOKEN_URL = BASE_API + "/oauth2/token";
 	/** OAuth2 Token破棄 */
 	public static final String TOKEN_REVOKE_URL = BASE_API + "/oauth2/token/revoke";
+
 }
